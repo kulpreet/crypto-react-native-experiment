636742ffdbbc3002d62f49f56e012cefbb1ff388
Kulpreet Singh <zapfmann@gmail.com>
2018-01-25 19:25:02 +0100

    Trying bitcoinjs with rn-denodeify

--- App.js
+++ App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import CreateChluTransaction from 'chlu-wallet-support-js';
+require('./shim');
+var bitcoin = require('bitcoinjs-lib')
+var bip39 = require('bip39')
 
 
 export default class App extends React.Component {
   
   render() {
-    let amount = 1e4
-    let txBuilder = new CreateChluTransaction()  
-    let mnemonic = "alter ankle cart harvest ecology sign athlete congress desert scare planet love"
-    txBuilder.getImportedKey(mnemonic)
+    var mnemonic = bip39.generateMnemonic()
+    
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>mnemonic</Text>
+        <Text>2Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
