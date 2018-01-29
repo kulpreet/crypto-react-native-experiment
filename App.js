2db31f8f0d851bb0f6ba3d722a4a81bd3b4e158c
Kulpreet Singh <zapfmann@gmail.com>
2018-01-29 19:17:23 +0100

    Make secure random work inside react native
    Used to generate mnemonic for now

--- App.js
+++ App.js
@@ -3,16 +3,32 @@ import { StyleSheet, Text, View } from 'react-native';
 require('./shim');
 var bitcoin = require('bitcoinjs-lib')
 var bip39 = require('bip39')
-
+import { generateSecureRandom } from 'react-native-securerandom';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {mnemonic: ''};
+  }
+  
+  componentDidMount() {
+    let _this = this
+    generateSecureRandom(128/8).then(randomBytes => {
+      console.log(randomBytes)
+      mnemonic = bip39.entropyToMnemonic(randomBytes)
+      _this.setState({mnemonic: mnemonic});
+      console.log(mnemonic)
+    }).catch(err => {
+      console.log(err)
+    });
+  }
   
   render() {
-    var mnemonic = bip39.generateMnemonic()
     
     return (
       <View style={styles.container}>
-        <Text>mnemonic</Text>
+        <Text>{this.state.mnemonic}</Text>
         <Text>2Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
